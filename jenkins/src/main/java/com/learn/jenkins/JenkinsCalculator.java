476b2c8f136e6a5d72f84bb791223cc340d917be
Dinesh Arumugam <dinesh.arumugam@pearson.com>
2020-07-09 22:33:59 +0530

    maven build

---
+++ jenkins/src/main/java/com/learn/jenkins/JenkinsCalculator.java
@@ -0,0 +1,13 @@
+package com.learn.jenkins;
+
+public class JenkinsCalculator {
+	
+  //Method to add 2 numbers
+  public int addNumbers(int numberOne, int numberTwo){
+	  return numberOne + numberTwo;
+  }
+  //Method to add 2 numbers
+  public int subtractNumbers(int numberOne, int numberTwo){
+	  return numberOne - numberTwo;
+  }
+}
